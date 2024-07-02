17579d14563d6e160cb965835d782849234c0095
Sumit Bondd <168127092+sumitbondd@users.noreply.github.com>
2024-07-02 20:11:25 +0000

    updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'sumitbondd',
+    author_email= 'sumityad007@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
